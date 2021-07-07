f0f4b928efc106e6194661bbede5a107b0a5131d
Beyond-Logic <edemaero@gmail.com>
2021-07-07 23:14:54 +0100

    files update

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import CheckOutLayout from '../components/CheckOutLayout';
-import BackToTop from '../components/BackToTop';
-import Footer from '../components/Footer';
+import React from "react";
+import CheckOutLayout from "../components/CheckOutLayout";
+import BackToTop from "../components/BackToTop";
+import Footer from "../components/Footer";
 
 function Checkout() {
-    return (
-        <>
-        <CheckOutLayout />
-        <BackToTop />
-        <Footer />
-        </>
-    )
+  return (
+    <>
+      <CheckOutLayout />
+      <BackToTop />
+      <Footer />
+    </>
+  );
 }
 
-export default Checkout
+export default Checkout;
